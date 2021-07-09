cecc72b5c0f5d9244e687c042cee46922c85624e
prabhu <prabhu.shekar@ee.co.uk>
2021-07-09 21:21:57 +0100

    test data fix

--- src/data/test.data.js
+++ src/data/test.data.js
@@ -1,5 +1,5 @@
 module.exports = {
-	url: 'https://id.ee.co.uk/id/login',
+	'url': 'https://id.ee.co.uk/id/login',
 	'PAYM Legal Owner User': 'appautonofg@yopmail.com',
-	password: 'Nttdata@123',
+	'password': 'Nttdata@123',
 };
