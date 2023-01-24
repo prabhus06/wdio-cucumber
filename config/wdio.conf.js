fed62b0cefa4eda4bc5788c85bad3139e4ac50bc
prabhus06 <prabhu.shekar@ee.co.uk>
2023-01-24 16:48:29 +0000

    updated dependencies

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -25,7 +25,7 @@ exports.config = {
 	// then the current working directory is where your `package.json` resides, so `wdio`
 	// will be called from there.
 	//
-	specs: ['./src/features/**/*.feature'],
+	specs: ['./src/features/*.feature'],
 
 	// Patterns to exclude.
 	exclude: [
